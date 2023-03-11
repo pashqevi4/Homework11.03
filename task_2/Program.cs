617e45a3a65e80cf7677a7d6ee8de4c4bc4dfa78
pashqa <pashqevi4@gmail.com>
2023-03-11 22:39:24 +0700

    Second task complete

---
+++ task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая показывает третью цифру заданного числа 
+//или сообщает, что третьей цифры нет
+int a = new Random().Next(1,100000000);
+Console.WriteLine(a);
+if (a < 100)
+{
+    Console.WriteLine("There's no third digit");
+   }
+  
+if ((a < 1000)&&(a>100))
+{
+    int x = a % 10;
+    Console.WriteLine(x);
+}
+
+if (a > 1000)
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    int y = a % 10;
+    Console.WriteLine(y);
+}
