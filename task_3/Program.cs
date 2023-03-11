fea4c7b66386b5aef5fa7763df2054f70781cbe2
pashqa <pashqevi4@gmail.com>
2023-03-11 22:39:34 +0700

    Third task complete

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter day of the week");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 7)
+{
+    Console.WriteLine("There's no such a day");
+}
+if ((a > 5)&&(a < 8))
+{
+    Console.WriteLine("Hooray! It's weekend! (YES)");
+}
+if (a < 6)
+Console.WriteLine("Get back to work! (NO)");
